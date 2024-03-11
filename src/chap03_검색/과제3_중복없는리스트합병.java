4566acc6fb2ed874a0bec67edb783092c12caba1
LeeYeongin <youngin5757@gmail.com>
2024-03-12 00:43:44 +0900

    과제 3 중복없는 리스트 합병

--- src/chap03_검색/과제3_중복없는리스트합병.java
+++ src/chap03_검색/과제3_중복없는리스트합병.java
@@ -4,6 +4,7 @@ package chap03_검색;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
@@ -22,24 +23,68 @@ public class 과제3_중복없는리스트합병 {
 //file2: 런던, 로마,방콕, 도쿄,서울,부산
 //file > string split() > 배열 > ArrayList > sort > iterator 사용하여 merge > 중복 제거 > string 배열 > file에 저장
 
-//	static ArrayList<String> removeDuplicate(ArrayList<String> al) {
-//		//구현할 부분 : 리스트에서 중복을 제거한다, 정렬후 호출하는 것을 전제로 구현
-//
-//
-//		return list1;
-//	}
+	static ArrayList<String> removeDuplicate(ArrayList<String> al) {
+		//구현할 부분 : 리스트에서 중복을 제거한다, 정렬후 호출하는 것을 전제로 구현
+		int cnt = 0;
+		for(int i=0; i<al.size(); i++) {
+			cnt = Collections.frequency(al, al.get(i));
+			if(cnt > 1) {
+				cnt--;
+				while(cnt > 0) {
+					al.remove(al.get(i));
+					cnt--;
+				}
+			}
+		}
+		
+		return al;
+	}
 	
 	static void trimSpace(String[]arr) {
-		
-	}
-	static void makeList(String[] sarray1, List<String>list1) {
-		
+		for(int i=0; i<arr.length; i++) {
+			arr[i]=arr[i].replaceAll(" ", "");
+		}
 	}
 	
-//	static List<String> mergeList(List<String> list1, List<String> list2) {
-//		ArrayList<String> list3 = new ArrayList<>();
-//		
-//	}
+	static void makeList(String[] sarray1, List<String>list1) {
+		for(String st : sarray1) {
+			list1.add(st);
+		}
+	}
+	
+	static List<String> mergeList(List<String> list1, List<String> list2) {
+		ArrayList<String> list3 = new ArrayList<>();
+		
+		int i = 0, j = 0;
+		while(i < list1.size() || j < list2.size()) {
+			if(i == list1.size()) {
+				while(j < list2.size()) {
+					list3.add(list2.get(j));
+					j++;
+				}
+			} else if(j == list2.size()) {
+				while(i < list1.size()) {
+					list3.add(list1.get(i));
+					i++;
+				}
+			} else {
+				if(list1.get(i).compareTo(list2.get(j)) > 0) {
+					list3.add(list2.get(j));
+					j++;
+				} else if(list1.get(i).compareTo(list2.get(j)) < 0) {
+					list3.add(list1.get(i));
+					i++;
+				} else {
+					list3.add(list1.get(i));
+					i++;
+					j++;
+				}
+			}
+		}
+		
+		return list3;
+		
+	}
 	public static void main(String[] args) {
 		try {
 			Path input1 = Paths.get("a1.txt");
@@ -50,69 +95,80 @@ public class 과제3_중복없는리스트합병 {
 			
 			String s1 = new String(bytes1);
 			String s2 = new String(bytes2);
+			
 			System.out.println("입력 스트링: s1 = " + s1);
 			System.out.println("입력 스트링: s2 = " + s2);
 			String[] sarray1 = s1.split("[,\\s]+\r\n");// 자바 regex \n으로 검색
 			String[] sarray2 = s2.split("[,\\s]+\r\n");//file에서 enter키는 \r\n으로 해야 분리됨
+
 			showData("스트링 배열 sarray1", sarray1);
 			showData("스트링 배열 sarray2", sarray2);
-//
-//			trimSpace(sarray1);
-//			trimSpace(sarray2);
-//
-//			showData("trimSpace() 실행후 :스트링 배열 sarray1", sarray1);
-//			showData("trimSpace() 실행후 :스트링 배열 sarray2", sarray2);
-//			System.out.println("+++++++\n");
-//			// file1에서 read하여 list1.add()한다.
-//			// 배열을 list로 만드는 방법
-//			// 방법1:
-//			ArrayList<String> list1 = new ArrayList<>();
-//			makeList(sarray1, list1);
-//			showList("리스트1: ", list1);
-//			// 방법2
-//			ArrayList<String> list2 = new ArrayList<>(Arrays.asList(sarray2));
-//			showList("리스트2: ", list2);
-//			
-//			System.out.println("+++++++ collection.sort()::");
-//			Collections.sort(list1);
-//			showList("정렬후 리스트1: ", list1);
-//
-//			//Arrays.sort(list2, null);//에러 발생 확인하고 이유는?
-//			Collections.sort(list2);
-//			showList("정렬후 리스트2: ", list2);	
-//	
-//			//정렬된 list에서 중복 제거 코드
-//			list1 = removeDuplicate(list1);
-//			list2 = removeDuplicate(list2);
-//			showList("중복 제거후 리스트1: ", list1);	
-//			showList("중복 제거후 리스트1: ", list1);	
-//	
-//	
-//			List<String> list3 = new ArrayList<>();
-//			
-//			// 방법3:
-//			list3 = mergeList(list1, list2);
-//			showList("merge후 합병리스트: ", list3);	
-//
-//			// ArrayList를 배열로 전환
-//			String[] st = list3.toArray(new String[list3.size()]);
-//			// binary search 구현
-//			// binSearch(st, st.length, "key");
-//			// 정렬된 list3을 file에 출력하는 코드 완성
-//			System.out.println("\n" + "file에 출력:");
-//			int bufferSize = 10240;
-//			
-//			ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
-//			writeFile(list3, buffer);
-//			
-//			FileOutputStream file = new FileOutputStream("c.txt");
-//			FileChannel channel = file.getChannel();
-//			channel.write(buffer);
-//			file.close();
+
+			trimSpace(sarray1);
+			trimSpace(sarray2);
+
+			showData("trimSpace() 실행후 :스트링 배열 sarray1", sarray1);
+			showData("trimSpace() 실행후 :스트링 배열 sarray2", sarray2);
+			System.out.println("+++++++\n");
+			
+			// file1에서 read하여 list1.add()한다.
+			// 배열을 list로 만드는 방법
+			// 방법1:
+			ArrayList<String> list1 = new ArrayList<>();
+			makeList(sarray1, list1);
+			showList("리스트1: ", list1);
+
+			// 방법2
+			ArrayList<String> list2 = new ArrayList<>(Arrays.asList(sarray2));
+			showList("리스트2: ", list2);
+			
+			System.out.println("+++++++ collection.sort()::");
+			Collections.sort(list1);
+			showList("정렬후 리스트1: ", list1);
+
+//			Arrays.sort(list2, null);//에러 발생 확인하고 이유는? ->  ArrayList는 배열 기반이 아닌 동적인 크기를 가진 리스트이므로 사용 불가
+			Collections.sort(list2);
+			showList("정렬후 리스트2: ", list2);	
+	
+			//정렬된 list에서 중복 제거 코드
+			list1 = removeDuplicate(list1);
+			list2 = removeDuplicate(list2);
+			showList("중복 제거후 리스트1: ", list1);	
+			showList("중복 제거후 리스트2: ", list2);	
+
+			List<String> list3 = new ArrayList<>();
+			
+			// 방법3:
+			list3 = mergeList(list1, list2);
+			showList("merge후 합병리스트: ", list3);	
+
+			// ArrayList를 배열로 전환
+			String[] st = list3.toArray(new String[list3.size()]);
+			// binary search 구현
+			//binarySearch(st, st.length, "key");
+			// 정렬된 list3을 file에 출력하는 코드 완성
+			System.out.println("\n" + "file에 출력:");
+			int bufferSize = 10240;
+			
+			ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+			writeFile(list3, buffer);
+			
+			FileOutputStream file = new FileOutputStream("c.txt");
+			FileChannel channel = file.getChannel();
+			channel.write(buffer);
+			file.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	private static void showList(String string, List<String> list1) {
+		System.out.println(string);
+		for(String st: list1) {
+			System.out.println(st);
+		}
+	}
+	
 	private static void showData(String string, String[] sarray1) {
 		System.out.println(string);
 		for(String s:sarray1) {
@@ -120,6 +176,7 @@ public class 과제3_중복없는리스트합병 {
 		}
 	
 	}
+	
 	static void writeFile(List<String> list3, ByteBuffer buffer) {
 		String b = " ";
 		for (String sx : list3) {
