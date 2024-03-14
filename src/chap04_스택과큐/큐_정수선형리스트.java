3aee76b2492ee853a72da639e512de5a6f8a610b
LeeYeongin <youngin5757@gmail.com>
2024-03-14 16:04:17 +0900

    정수선형리스트 큐

--- src/chap04_스택과큐/큐_정수선형리스트.java
+++ src/chap04_스택과큐/큐_정수선형리스트.java
@@ -3,6 +3,8 @@ package chap04_스택과큐;
 //선형 큐 구현
 import java.util.Scanner;
 
+import chap04_스택과큐.Queue4.EmptyQueueException;
+
 /*
  * Queue of ArrayList
  */
@@ -17,48 +19,87 @@ class Queue4 {
 	private int capacity; // 큐의 크기
 	private int front; // 맨 처음 요소 커서
 	private int rear; // 맨 끝 요소 커서
-	private int num; // 현재 데이터 개수
+//	private int num; // 현재 데이터 개수
 
-//--- 실행시 예외: 큐가 비어있음 ---//
+	//--- 실행시 예외: 큐가 비어있음 ---//
+	public class EmptyQueueException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
 
-//--- 실행시 예외: 큐가 가득 찼음 ---//
+		public EmptyQueueException(String msg) {
+			super(msg);
+		}
+	}
+	
+	//--- 실행시 예외: 큐가 가득 찼음 ---//
+	public class OverflowQueueException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
 
-//--- 생성자(constructor) ---//
+		public OverflowQueueException(String msg) {
+			super(msg);
+		}
+	}
+	
+	//--- 생성자(constructor) ---//
 	public Queue4(int maxlen) {
-
+		que = new ArrayList<Integer>();
+		capacity = maxlen;
+		front = rear = 0;
 	}
 
-//--- 큐에 데이터를 인큐 ---//
+	//--- 큐에 데이터를 인큐 ---//
 	public int enque(int x) throws OverflowQueueException {
 		if (isFull())
-
+			throw new OverflowQueueException("enque: Queue is full");
+		
+		rear++;
+		que.add(x);
+		
+		return x;
 	}
 
-//--- 큐에서 데이터를 디큐 ---//
+	//--- 큐에서 데이터를 디큐 ---//
 	public int deque() throws EmptyQueueException {
 		if (isEmpty())
-
+			throw new EmptyQueueException("enque: Queue is empty");
+		
+		rear--;
+		
+		return que.remove(front);
 	}
 
-//--- 큐에서 데이터를 피크(프런트 데이터를 들여다봄) ---//
+	//--- 큐에서 데이터를 피크(프런트 데이터를 들여다봄) ---//
 	public int peek() throws EmptyQueueException {
 		if (isEmpty())
-
+			throw new EmptyQueueException("peek: Queue is empty");
+		
+		return que.get(front);
 	}
 
-//--- 큐를 비움 --- peek() 처럼 예외 발생 구현//
+	//--- 큐를 비움 --- peek() 처럼 예외 발생 구현//
 	public void clear() throws EmptyQueueException {
 		/*
 		 * queue을 empty로 만들어야 한다.
 		 * queue이 empty일 때 clear()가 호출된 예외 발생해야 한다 
 		 */
 		if (isEmpty()) // queue이 빔
-
+			throw new EmptyQueueException("clear: Queue is already emtpy");
+		
+		rear = 0;
+		que.removeAll(que);
+	}
+	
+	//--- 큐 안의 모든 요소 출력 --- //
+	public void dump() throws EmptyQueueException {
+		if (isEmpty())
+			throw new EmptyQueueException("dump: Nothing to print(Queue is empty)");
+		
+		for(int v : que)
+			System.out.print(v + " ");
 	}
 
-//--- 큐에서 x를 검색하여 인덱스(찾지 못하면 –1)를 반환 ---//
+	//--- 큐에서 x를 검색하여 인덱스(찾지 못하면 –1)를 반환 ---//
 	public int indexOf(int x) {
-		for (int i = 0; i < num; i++) {
+		for (int i = 0; i < size(); i++) {
 			int idx = (i + front) % capacity;
 			if (que.get(idx) == x) // 검색 성공
 				return idx;
@@ -66,29 +107,29 @@ class Queue4 {
 		return -1; // 검색 실패
 	}
 
-//--- 큐의 크기를 반환 ---//
+	//--- 큐의 크기를 반환 ---//
 	public int getCapacity() {
 		return capacity;
 	}
 
-//--- 큐에 쌓여 있는 데이터 개수를 반환 ---//
+	//--- 큐에 쌓여 있는 데이터 개수를 반환 ---//
 	public int size() {
-		return num;
+		return rear-front;
 	}
 
-//--- 큐가 비어있는가? ---//
+	//--- 큐가 비어있는가? ---//
 	public boolean isEmpty() {
-		return num <= 0;
+		return (rear-front) <= 0;
 	}
 
-//--- 큐가 가득 찼는가? ---//
+	//--- 큐가 가득 찼는가? ---//
 	public boolean isFull() {
-		return num >= capacity;
+		return (rear-front) >= capacity;
 	}
 
 }
 
-public class 실습4_4정수선형큐_리스트 {
+public class 큐_정수선형리스트 {
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
 		Queue4 oq = new Queue4(4); // 최대 64개를 인큐할 수 있는 큐
@@ -99,6 +140,8 @@ public class 실습4_4정수선형큐_리스트 {
 			System.out.printf("현재 데이터 개수: %d / %d\n", oq.size(), oq.getCapacity());
 			System.out.print("(1)인큐　(2)디큐　(3)피크　(4)덤프　(5)clear  (0)종료: ");
 			int menu = stdIn.nextInt();
+			if (menu == 0)
+				break;
 			switch (menu) {
 			case 1: // 인큐
 				rndx = random.nextInt(20);
@@ -128,10 +171,18 @@ public class 실습4_4정수선형큐_리스트 {
 				}
 				break;
 			case 4: // 덤프
-
+				try {
+					oq.dump();
+				} catch (Queue4.EmptyQueueException e) {
+					System.out.println(e.getMessage());
+				}
 				break;
 			case 5: // clear
-
+				try {
+					oq.clear();
+				} catch (Queue4.EmptyQueueException e) {
+					System.out.println(e.getMessage());
+				}
 				break;
 			default:
 				break;
